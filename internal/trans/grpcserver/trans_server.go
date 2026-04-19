62e8fe8f2b308048aeca256bf936268e9c9afc10
SlayerSv <slayer-sv@mail.ru>
2026-04-19 15:16:02 +0700

    add transaction service and grpc server logic

---
+++ internal/trans/grpcserver/trans_server.go
@@ -0,0 +1,69 @@
+package grpc
+
+import (
+	"context"
+
+	"github.com/google/uuid"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+
+	"github.com/SlayerSv/payments/gen/trans"
+	"github.com/SlayerSv/payments/internal/trans/models"
+	"github.com/SlayerSv/payments/internal/trans/service"
+)
+
+type TransServer struct {
+	trans.UnimplementedTransServiceServer
+	service service.Transaction
+}
+
+func NewTransServer(svc service.Transaction) *TransServer {
+	return &TransServer{service: svc}
+}
+
+// Вспомогательная функция для маппинга енамов
+func mapProtoType(p trans.AccountType) models.AccountType {
+	switch p {
+	case trans.AccountType_SAVINGS:
+		return models.AccountSavings
+	case trans.AccountType_WALLET:
+		return models.AccountWallet
+	}
+	return models.AccountInvalid
+}
+
+func (s *TransServer) Deposit(ctx context.Context, req *trans.DepositRequest) (*trans.DepositResponse, error) {
+	userID, err := uuid.Parse(req.UserId)
+	if err != nil {
+		return nil, status.Error(codes.InvalidArgument, "invalid user_id format")
+	}
+	_, err = s.service.Deposit(ctx, userID, mapProtoType(req.AccType), req.Amount)
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "deposit failed: %v", err)
+	}
+	return &trans.DepositResponse{Status: "ok"}, nil
+}
+
+func (s *TransServer) Withdraw(ctx context.Context, req *trans.WithdrawRequest) (*trans.WithdrawResponse, error) {
+	userID, err := uuid.Parse(req.UserId)
+	if err != nil {
+		return nil, status.Error(codes.InvalidArgument, "invalid user_id format")
+	}
+	_, err = s.service.Withdraw(ctx, userID, mapProtoType(req.AccType), req.Amount)
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "withdraw failed: %v", err)
+	}
+	return &trans.WithdrawResponse{Status: "ok"}, nil
+}
+
+func (s *TransServer) Transfer(ctx context.Context, req *trans.TransferRequest) (*trans.TransferResponse, error) {
+	senderID, err := uuid.Parse(req.SenderId)
+	if err != nil {
+		return nil, status.Error(codes.InvalidArgument, "invalid sender_id format")
+	}
+	_, err = s.service.Transfer(ctx, senderID, mapProtoType(req.SenderAccType), req.ReceiverEmail, mapProtoType(req.ReceiverAccType), req.Amount)
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "transfer failed: %v", err)
+	}
+	return &trans.TransferResponse{Status: "ok"}, nil
+}
---
+++ internal/trans/service/trans.go
@@ -0,0 +1,83 @@
+package service
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/google/uuid"
+
+	"github.com/SlayerSv/payments/internal/shared/errs"
+	"github.com/SlayerSv/payments/internal/trans/models"
+	"github.com/SlayerSv/payments/internal/trans/repository"
+)
+
+type Transaction struct {
+	repo repository.Transaction
+}
+
+func NewTransaction(repo repository.Transaction) *Transaction {
+	return &Transaction{
+		repo: repo,
+	}
+}
+
+func (s *Transaction) Deposit(ctx context.Context, user_id uuid.UUID, accType models.AccountType, amount int64) (uuid.UUID, error) {
+	if amount <= 0 {
+		return uuid.Nil, fmt.Errorf("%w: amount must be positive", errs.BadRequest)
+	}
+	if accType == models.AccountInvalid {
+		return uuid.Nil, fmt.Errorf("%w: invalid account type", errs.BadRequest)
+	}
+	tx := models.Transaction{
+		ReceiverID:   user_id,
+		ReceiverType: accType,
+		Amount:       amount,
+		OpType:       models.OperationDeposit,
+	}
+	return s.repo.Create(ctx, tx)
+}
+
+func (s *Transaction) Withdraw(ctx context.Context, user_id uuid.UUID, accType models.AccountType, amount int64) (uuid.UUID, error) {
+	if amount <= 0 {
+		return uuid.Nil, fmt.Errorf("%w: amount must be positive", errs.BadRequest)
+	}
+	if accType == models.AccountInvalid {
+		return uuid.Nil, fmt.Errorf("%w: invalid account type", errs.BadRequest)
+	}
+	tx := models.Transaction{
+		SenderID:   user_id,
+		SenderType: accType,
+		Amount:     amount,
+		OpType:     models.OperationWithdraw,
+	}
+	return s.repo.Create(ctx, tx)
+}
+
+func (s *Transaction) Transfer(
+	ctx context.Context,
+	senderID uuid.UUID,
+	senderAccType models.AccountType,
+	receiverEmail string,
+	receiverAccType models.AccountType,
+	amount int64) (uuid.UUID, error) {
+	if amount <= 0 {
+		return uuid.Nil, fmt.Errorf("%w: amount must be positive", errs.BadRequest)
+	}
+	if senderAccType == models.AccountInvalid {
+		return uuid.Nil, fmt.Errorf("%w: invalid account type", errs.BadRequest)
+	}
+	var receiverID uuid.UUID
+	tx := models.Transaction{
+		SenderID:     senderID,
+		SenderType:   senderAccType,
+		ReceiverID:   receiverID,
+		ReceiverType: receiverAccType,
+		Amount:       amount,
+		OpType:       models.OperationTransfer,
+	}
+	return s.repo.Create(ctx, tx)
+}
+
+func (s *Transaction) GetAccountHistory(ctx context.Context, accountID uuid.UUID) ([]models.Transaction, error) {
+	return s.repo.GetHistoryByAccount(ctx, accountID)
+}
