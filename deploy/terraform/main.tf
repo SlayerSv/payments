7af9d2360785bdf19b7c1080d3e36a2d0c20886c
SlayerSv <slayer-sv@mail.ru>
2026-06-23 00:22:47 +0700

    add env var

--- deploy/terraform/main.tf
+++ deploy/terraform/main.tf
@@ -1,187 +1,199 @@
 # ==============================================================================
-# 1. НАСТРОЙКА ПРОВАЙДЕРА CLOUD.RU ADVANCED
+# 1. НАСТРОЙКА ПРОВАЙДЕРА CLOUD.RU EVOLUTION
 # ==============================================================================
 terraform {
   required_providers {
-    sbercloud = {
-      source  = "sbercloud-terraform/sbercloud"
-      version = ">= 1.10.0"
+    cloudru = {
+      source  = "cloud.ru/cloudru/cloud" # Официальный провайдер Evolution
+      version = ">= 2.0.0"
     }
   }
 }
 
-provider "sbercloud" {
-  auth_url   = "https://iam.ru-moscow-1.hc.sbercloud.ru/v3" # Московский регион
-  region     = "ru-moscow-1"
-  access_key = var.cloud_access_key
-  secret_key = var.cloud_secret_key
+provider "cloudru" {
+  project_id  = var.project_id
+  auth_key_id = var.auth_key_id  # Твой Key ID (логин сервисного аккаунта)
+  auth_secret = var.auth_secret  # Твой Key Secret (пароль сервисного аккаунта)
 }
 
 # ==============================================================================
-# 2. ПЕРЕМЕННЫЕ (Входные параметры)
+# 2. ПЕРЕМЕННЫЕ
 # ==============================================================================
-variable "cloud_access_key" {
+variable "project_id" {
   type        = string
-  description = "Access Key (из личного кабинета Cloud.ru)"
+  description = "Идентификатор проекта в Cloud.ru Evolution"
 }
 
-variable "cloud_secret_key" {
+variable "auth_key_id" {
   type        = string
-  description = "Secret Key (из личного кабинета Cloud.ru)"
-  sensitive   = true # Скрывает пароль в логах терраформа
+  description = "Key ID сервисного аккаунта"
 }
 
-variable "existing_security_group_name" {
+variable "auth_secret" {
   type        = string
-  default     = "vkr-production-sg" # Замени на имя своей группы безопасности
-  description = "Имя уже созданной вручную группы безопасности"
+  sensitive   = true
+  description = "Key Secret сервисного аккаунта"
 }
 
-variable "existing_public_ip" {
+variable "existing_subnet_id" {
   type        = string
-  default     = "123.60.208.163" # Замени на свой белый статический IP
-  description = "Существующий арендованный белый IP для Master-ноды"
+  default     = "твоя-подсеть-id" # Скопируй ID подсети из консоли
+  description = "ID существующей подсети в VPC"
+}
+
+variable "existing_postgres_volume_id" {
+  type        = string
+  default     = "твой-диск-id" # Скопируй ID предсозданного диска EVS
+  description = "ID твоего существующего диска для базы данных"
 }
 
 # ==============================================================================
-# 3. ИСТОЧНИКИ ДАННЫХ (Существующие ресурсы в облаке)
+# 3. СЕТЕВЫЕ ИНТЕРФЕЙСЫ (В Evolution интерфейсы создаются отдельно)
 # ==============================================================================
 
-# Читаем настройки существующей группы безопасности
-data "sbercloud_networking_secgroup" "existing_sg" {
-  name = var.existing_security_group_name
+# Сетевой интерфейс для Master (VM-1)
+resource "cloudru_evolution_compute_interface" "master_interface" {
+  project_id = var.project_id
+  subnet_id  = var.existing_subnet_id
 }
 
-# Читаем настройки существующего белого IP адреса
-data "sbercloud_vpc_eip" "existing_master_eip" {
-  public_ip = var.existing_public_ip
+# Сетевой интерфейс для Worker (VM-2)
+resource "cloudru_evolution_compute_interface" "worker_interface" {
+  project_id = var.project_id
+  subnet_id  = var.existing_subnet_id
 }
 
-# ==============================================================================
-# 4. СОЗДАНИЕ СЕТИ (VPC и Подсеть)
-# ==============================================================================
-
-# Создаем Виртуальное Частное Облако (VPC)
-resource "sbercloud_vpc" "vkr_vpc" {
-  name = "vkr-payment-vpc"
-  cidr = "10.0.0.0/16"
+# Сетевой интерфейс для Data Node (VM-3)
+resource "cloudru_evolution_compute_interface" "data_interface" {
+  project_id = var.project_id
+  subnet_id  = var.existing_subnet_id
 }
 
-# Создаем подсеть 10.0.1.0/24 внутри VPC
-resource "sbercloud_vpc_subnet" "vkr_subnet" {
-  name       = "vkr-subnet"
-  vpc_id     = sbercloud_vpc.vkr_vpc.id
-  cidr       = "10.0.1.0/24"
-  gateway_ip = "10.0.1.1"
+# Сетевой интерфейс для Ops (VM-4)
+resource "cloudru_evolution_compute_interface" "ops_interface" {
+  project_id = var.project_id
+  subnet_id  = var.existing_subnet_id
 }
 
 # ==============================================================================
-# 5. СОЗДАНИЕ ВИРТУАЛЬНЫХ МАШИН (ECS)
+# 4. ВИРТУАЛЬНЫЕ МАШИНЫ (В Evolution это ресурс cloudru_evolution_compute_vm)
 # ==============================================================================
 
-# VM-1: Master / Control Plane (2 vCPU, 4GB RAM)
-resource "sbercloud_compute_instance" "vm_master" {
-  name              = "vm-1-master"
-  image_name        = "Ubuntu 22.04"
-  flavor_id         = "s2.medium.2" # 2 vCPU, 4GB RAM
-  security_groups   = [data.sbercloud_networking_secgroup.existing_sg.name]
-  availability_zone = "ru-moscow-1a"
+# VM-1: Master Node (2 vCPU, 4GB RAM) -> Сбалансированная
+resource "cloudru_evolution_compute_vm" "vm_master" {
+  project_id = var.project_id
+  name       = "vm-1-master"
 
-  network {
-    uuid = sbercloud_vpc_subnet.vkr_subnet.id
+  zone_identifier = {
+    name = "ru.AZ-2" # Твоя зона доступности из консоли
   }
-}
 
-# VM-2: Worker / Apps Node (4 vCPU, 8GB RAM)
-resource "sbercloud_compute_instance" "vm_worker" {
-  name              = "vm-2-worker"
-  image_name        = "Ubuntu 22.04"
-  flavor_id         = "s2.large.2" # 4 vCPU, 8GB RAM
-  security_groups   = [data.sbercloud_networking_secgroup.existing_sg.name]
-  availability_zone = "ru-moscow-1a"
-
-  network {
-    uuid = sbercloud_vpc_subnet.vkr_subnet.id
+  flavor_identifier = {
+    name = "gen-2-4" # 2 ядра, 4 ГБ оперативной памяти
   }
-}
 
-# VM-3: Data Node (4 vCPU, 8GB RAM)
-resource "sbercloud_compute_instance" "vm_data" {
-  name              = "vm-3-data"
-  image_name        = "Ubuntu 22.04"
-  flavor_id         = "s2.large.2" # 4 vCPU, 8GB RAM
-  security_groups   = [data.sbercloud_networking_secgroup.existing_sg.name]
-  availability_zone = "ru-moscow-1a"
-
-  network {
-    uuid = sbercloud_vpc_subnet.vkr_subnet.id
+  image = {
+    id = "ubuntu-22-04-id" # ID образа Ubuntu 22.04 из твоего каталога
   }
+
+  network_interfaces = [
+    {
+      interface_id = cloudru_evolution_compute_interface.master_interface.id
+    }
+  ]
 }
 
-# VM-4: Ops / Monitoring Node (4 vCPU, 8GB RAM)
-resource "sbercloud_compute_instance" "vm_ops" {
-  name              = "vm-4-ops"
-  image_name        = "Ubuntu 22.04"
-  flavor_id         = "s2.large.2" # 4 vCPU, 8GB RAM
-  security_groups   = [data.sbercloud_networking_secgroup.existing_sg.name]
-  availability_zone = "ru-moscow-1a"
+# VM-2: Worker Node (2 vCPU, 8GB RAM) -> Сбалансированная (как твоя тестовая)
+resource "cloudru_evolution_compute_vm" "vm_worker" {
+  project_id = var.project_id
+  name       = "vm-2-worker"
 
-  network {
-    uuid = sbercloud_vpc_subnet.vkr_subnet.id
+  zone_identifier = {
+    name = "ru.AZ-2"
   }
+
+  flavor_identifier = {
+    name = "gen-2-8" # 2 ядра, 8 ГБ оперативной памяти (тот самый флейвор!)
+  }
+
+  image = {
+    id = "ubuntu-22-04-id"
+  }
+
+  network_interfaces = [
+    {
+      interface_id = cloudru_evolution_compute_interface.worker_interface.id
+    }
+  ]
+}
+
+# VM-3: Data Node (2 vCPU, 8GB RAM + ПОДКЛЮЧЕННЫЙ ОБЛАЧНЫЙ ДИСК)
+resource "cloudru_evolution_compute_vm" "vm_data" {
+  project_id = var.project_id
+  name       = "vm-3-data"
+
+  zone_identifier = {
+    name = "ru.AZ-2"
+  }
+
+  flavor_identifier = {
+    name = "gen-2-8"
+  }
+
+  image = {
+    id = "ubuntu-22-04-id"
+  }
+
+  network_interfaces = [
+    {
+      interface_id = cloudru_evolution_compute_interface.data_interface.id
+    }
+  ]
+
+  # В Evolution диски монтируются прямо внутри ресурса VM! Очень удобно:
+  disks = [
+    {
+      disk_id = var.existing_postgres_volume_id
+    }
+  ]
+}
+
+# VM-4: Ops Node (2 vCPU, 8GB RAM)
+resource "cloudru_evolution_compute_vm" "vm_ops" {
+  project_id = var.project_id
+  name       = "vm-4-ops"
+
+  zone_identifier = {
+    name = "ru.AZ-2"
+  }
+
+  flavor_identifier = {
+    name = "gen-2-8"
+  }
+
+  image = {
+    id = "ubuntu-22-04-id"
+  }
+
+  network_interfaces = [
+    {
+      interface_id = cloudru_evolution_compute_interface.ops_interface.id
+    }
+  ]
 }
 
 # ==============================================================================
-# 6. СВЯЗЫВАНИЕ БЕЛОГО IP С MASTER-НОДОЙ
+# 5. ВЫХОДНЫЕ ПАРАМЕТРЫ ДЛЯ ANSIBLE
 # ==============================================================================
-resource "sbercloud_compute_eip_associate" "associate_master" {
-  public_ip   = data.sbercloud_vpc_eip.existing_master_eip.address
-  instance_id = sbercloud_compute_instance.vm_master.id
+output "master_ip" {
+  value = cloudru_evolution_compute_interface.master_interface.ip_address
 }
-
-# ==============================================================================
-# 7. ПОСТОЯННЫЙ ОБЛАЧНЫЙ ДИСК ДЛЯ БАЗЫ ДАННЫХ (EVS)
-# ==============================================================================
-
-# Создаем диск EVS на 20 ГБ
-resource "sbercloud_evs_volume" "postgres_disk" {
-  name              = "vkr-postgres-persistent-disk"
-  availability_zone = "ru-moscow-1a" # Должна совпадать с зоной VM-3
-  volume_type       = "SAS"             # Надежный и стандартный тип диска
-  size              = 20
+output "worker_ip" {
+  value = cloudru_evolution_compute_interface.worker_interface.ip_address
 }
-
-# Прикрепляем созданный диск к VM-3 (Data Node)
-resource "sbercloud_compute_volume_attach" "postgres_disk_attach" {
-  instance_id = sbercloud_compute_instance.vm_data.id
-  volume_id   = sbercloud_evs_volume.postgres_disk.id
+output "data_ip" {
+  value = cloudru_evolution_compute_interface.data_interface.ip_address
 }
-
-# ==============================================================================
-# 8. ВЫХОДНЫЕ ПАРАМЕТРЫ (Облегчают настройку Ansible)
-# ==============================================================================
-
-output "vm_1_master_public_ip" {
-  value       = data.sbercloud_vpc_eip.existing_master_eip.public_ip
-  description = "Публичный IP адрес для подключения по SSH"
-}
-
-output "vm_1_master_private_ip" {
-  value       = sbercloud_compute_instance.vm_master.access_ip_v4
-  description = "Внутренний IP-адрес Master-ноды"
-}
-
-output "vm_2_worker_private_ip" {
-  value       = sbercloud_compute_instance.vm_worker.access_ip_v4
-  description = "Внутренний IP-адрес Worker-ноды (Apps)"
-}
-
-output "vm_3_data_private_ip" {
-  value       = sbercloud_compute_instance.vm_data.access_ip_v4
-  description = "Внутренний IP-адрес Data-ноды (Postgres + Kafka)"
-}
-
-output "vm_4_ops_private_ip" {
-  value       = sbercloud_compute_instance.vm_ops.access_ip_v4
-  description = "Внутренний IP-адрес Ops-ноды (Monitoring)"
+output "ops_ip" {
+  value = cloudru_evolution_compute_interface.ops_interface.ip_address
 }
